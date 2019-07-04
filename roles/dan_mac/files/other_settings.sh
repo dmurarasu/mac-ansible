cd2c32f4e830313d5c2804b36e4ea1226a0353ea
Dan Murarasu <murarasd@01827-DT-MurarasD.local>
2019-07-04 11:24:55 +0100

    first dan draft

---
+++ roles/dan_mac/files/other_settings.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+### using aws-azure-login with docker
+sudo curl -o /usr/local/bin/aws-azure-login https://raw.githubusercontent.com/dtjohnson/aws-azure-login/master/docker-launch.sh
+sudo chmod o+x /usr/local/bin/aws-azure-login
+sudo chmod go+x /usr/local/bin/aws-azure-login
