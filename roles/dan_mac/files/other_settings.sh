de3db8866e14ffcd9d0b0bdec5c147a1f6a28554
Dan Murarasu <dan.murarasu@gmail.com>
2019-07-04 11:46:56 +0100

    added git

--- roles/dan_mac/files/other_settings.sh
+++ roles/dan_mac/files/other_settings.sh
@@ -5,3 +5,7 @@ sudo curl -o /usr/local/bin/aws-azure-login https://raw.githubusercontent.com/dt
 sudo chmod o+x /usr/local/bin/aws-azure-login
 sudo chmod go+x /usr/local/bin/aws-azure-login
 
+### configuring git user and email and to use token from keychain(add token to keychain)
+git config --global user.name "Dan Murarasu"
+git config --global user.email "dan.murarasu@gmail.com"
+git config --global credential.helper=osxkeychain
